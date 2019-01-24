ce03d94e4fb61d2d362a88e19ce20b2587d2b1a5
Damjan Janevski <janevskidamjan@yahoo.com>
2019-01-24 23:09:40 +0100

    Initial Commit

---
+++ DealApp.API/Data/DataContext.cs
@@ -0,0 +1,13 @@
+using DealApp.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DealApp.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext (DbContextOptions<DataContext> options) : base(options) {}
+
+
+        public DbSet<Value> Values { get; set; }
+    }
+}
